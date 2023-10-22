f8052da385e2bcf8806973a1183a5e1d3d6e10e8
sadohu <sadohu26@yahoo.com>
2023-10-21 21:10:36 -0500

    init project

---
+++ src/main/java/com/cibertec/iosproject/IosProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.iosproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IosProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IosProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/iosproject/IosProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.iosproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IosProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
